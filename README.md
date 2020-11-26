7a330a0ccb7b0f3f983bfd6d71e32fac0f0ac44d
Logan Zehr <zehrl315@outlook.com>
2020-11-26 10:38:43 -0800

    Basic pseudo code

---
+++ README.md
@@ -0,0 +1,19 @@
+<!-- 
+[] (license badge!)
+
+[] Title of Project
+[] Table of Contents (no input, works with clicking)
+[] Description
+[] Instillation
+[] Usage
+[] License (auto generated)
+[] Contributing
+[] Tests
+[] Questions
+    -contains link to github account
+    -email and instructions for reaching out
+ -->
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+ 
+ # Title of Project
+
--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+// Include modules
+const { error } = require("console")
 const fs = require("fs")
+const inquirer = require('inquirer')
+
+// Prompt User
+inquirer.prompt([
+    // title of project
+    // description
+    // installation
+    // usage
+    // contributing
+    // tests
+    // license (from a list!) https://choosealicense.com/licenses/
+        //GNU AGPLv3, GNU GPLv3, GNU LGPLv3, Mozilla Public License 2.0, 
+        //Apache License 2.0, MIT License, Boost Software License 1.0, 
+        //The Unlicense
+    // username of github account
+    // email address of user
+    
+    
+    // {
+    //     type: "input",
+    //     message: "What is your name?",
+    //     name: "name",
+    // }
+])
+    .then((response) => {
+
+        // Pull license based on input
+            // findLicense()
+
+        // Readme text with Input from User
+        const text =
+            ``
+
+        // Write to README.md
+        fs.writeFile("README.md", text, (error) => {
+            error ? console.log(error) : console.log("README.md successfully updated.")
+        })
+
+    })
+
+
+const findLicense = license => {
+    switch (license) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
